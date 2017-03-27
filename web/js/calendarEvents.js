5703bc6df7a77ad902d75d598164e8f89bc28baa
Dominique Ross <dross5@radford.edu>
2017-03-27 12:16:19 -0400

    final

--- web/js/calendarEvents.js
+++ web/js/calendarEvents.js
@@ -5,7 +5,7 @@ function myFunction() {
         $.getJSON('https://www.googleapis.com/calendar/v3/calendars/kibrv4dalfnvj5u08vfacu98k8@group.calendar.google.com/events?key=AIzaSyD0g9mSQftiEARNlU8X7TbIYPCCoLKjagg', function(data) {
             for(i in data['items']) {
                 item = data['items'][i];
-                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Starting : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
+                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Time : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
 
             }
         });
@@ -15,7 +15,7 @@ function myFunction() {
         $.getJSON('https://www.googleapis.com/calendar/v3/calendars/apjsaqt68sa0fs0e6nmpo81ms0@group.calendar.google.com/events?key=AIzaSyD0g9mSQftiEARNlU8X7TbIYPCCoLKjagg', function(data) {
             for(i in data['items']) {
                 item = data['items'][i];
-                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Starting : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
+                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Time : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
             }
         });
     }
@@ -24,7 +24,7 @@ function myFunction() {
         $.getJSON('https://www.googleapis.com/calendar/v3/calendars/5e5no23jcqdbqmin32f8svdm8g@group.calendar.google.com/events?key=AIzaSyD0g9mSQftiEARNlU8X7TbIYPCCoLKjagg', function(data) {
             for(i in data['items']) {
                 item = data['items'][i];
-                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Starting : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
+                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Time : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
 
             }
         });
@@ -34,7 +34,7 @@ function myFunction() {
         $.getJSON('https://www.googleapis.com/calendar/v3/calendars/n09br9cegtalvd0m8b0k32ofs8@group.calendar.google.com/events?key=AIzaSyD0g9mSQftiEARNlU8X7TbIYPCCoLKjagg', function(data) {
             for(i in data['items']) {
                 item = data['items'][i];
-                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Starting : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
+                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Time : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
 
             }
         });
@@ -44,7 +44,7 @@ function myFunction() {
         $.getJSON('https://www.googleapis.com/calendar/v3/calendars/6ntp3er9c2t9cu8cckr0vibk4s@group.calendar.google.com/events?key=AIzaSyD0g9mSQftiEARNlU8X7TbIYPCCoLKjagg', function(data) {
             for(i in data['items']) {
                 item = data['items'][i];
-                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Starting : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
+                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Time : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
 
             }
         });
@@ -54,7 +54,7 @@ function myFunction() {
         $.getJSON('https://www.googleapis.com/calendar/v3/calendars/dirpjbv7sfq5p2v13r9g9gso04@group.calendar.google.com/events?key=AIzaSyD0g9mSQftiEARNlU8X7TbIYPCCoLKjagg', function(data) {
             for(i in data['items']) {
                 item = data['items'][i];
-                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Starting : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
+                $("#event-list").append("<div class='panel panel-danger' style='background-color: rgba(255, 0, 0, 0.74)'><div class='panel-body'>" + "<h2>"+ item.summary + "</h2><h3> Category: '" + data.summary + "'</h3><p>Time : "+item.start.dateTime + "</p><p>Location: " + item.location + "</p></div></div><br/>");
 
             }
         });
